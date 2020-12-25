bca93201f503a41fb5f3840872697111fd023592
Senko <66232594+Senbyte1@users.noreply.github.com>
2020-12-24 20:59:41 -0800

    Delete LF.js

--- Commands/LF.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports = {
-    name: "leaning",
-    run: async (client, message, args, print, prefix) => {
-		  message.channel.send("<@590286037323087882> Ping!\n LeaningDown is a furry!");
-    }
-}
